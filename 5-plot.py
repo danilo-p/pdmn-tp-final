4cc0f8851bdd3bcf7ac67e00cf8a28a0bd779441
Danilo Pimentel <danilo.pimentel97@gmail.com>
2021-03-28 19:41:25 -0300

    add q5 results

--- 5-plot.py
+++ 5-plot.py
@@ -2,12 +2,7 @@ import choropleth
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Local result
-df = pd.read_csv(
-    "./output/local_tp_final_5_plays_by_country_rdd.csv/all.csv", skipinitialspace=True)
-
-# Cluster result
-# df = pd.read_csv("./output/tp_final_5_plays_by_country_rdd.csv/all.csv", skipinitialspace=True)
+df = pd.read_csv("./5.csv", skipinitialspace=True)
 
 choropleth.plot_choropleth_map(df.values.tolist())
 
